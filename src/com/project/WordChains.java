9bc184af3fc6029b8f28c232eb946bee9c1848cd
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-09-15 23:37:57 +0200

    Remove unnecessary code

--- src/com/project/WordChains.java
+++ src/com/project/WordChains.java
@@ -6,199 +6,175 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordChains {
-    private static List<String> contentList;
+	   private static List<String> contentList;
 
-    private static String searchedWord;
+	    private static String searchedWord;
 
-    private static volatile boolean found;
+	    private static volatile boolean found;
 
-    private static List<String> path;
+	    private static List<String> path;
 
-    private static Map<String, Integer> wordOccured;
+	    private static Map<String, Integer> wordOccured;
 
+	    private static int countOccurencesValue;
 
-    private static int countOccurencesValue;
+	    public static void main(String[] args) {
 
-    public static void main(String[] args) {
+	        String[] startArr = {"cat", "gold", "code"};
+	        String[] endArr = {"dog", "lead", "ruby"};
 
-        String[] startArr = {"cat", "gold", "code"};
-        String[] endArr = {"dog", "lead", "ruby"};
+	        String startWord = startArr[1];
+	        String endWord = endArr[1];
 
-        String startWord = startArr[1];
-        String endWord = endArr[1];
+	        Callable<List<String>> readResources = new ReadFromURL("http://codekata.com/data/wordlist.txt", startWord.length(), startWord, endWord);
 
-        Callable<List<String>> readResources = new ReadFromURL("http://codekata.com/data/wordlist.txt", startWord.length(), startWord, endWord);
+	        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	        List<Future<List<String>>> listOfFutures = new ArrayList<>();
 
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        List<Future<List<String>>> listOfFutures = new ArrayList<>();
+	        Future<List<String>> oneFuture = executorService.submit(readResources);
+	        listOfFutures.add(oneFuture);
 
-        Future<List<String>> oneFuture = executorService.submit(readResources);
-        listOfFutures.add(oneFuture);
+	        List<String> oneCallableResult = null;
+	        try {
+	            oneCallableResult = oneFuture.get();
+	            for (String result : oneCallableResult) {
+	                System.out.println(result);
+	            }
 
-        List<String> oneCallableResult = null;
-        try {
-            oneCallableResult = oneFuture.get();
-            for (String result : oneCallableResult) {
-                System.out.println(result);
-            }
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        } catch (ExecutionException e) {
+	            e.printStackTrace();
+	        }
+	        executorService.shutdown();
+	        try {
+	            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        executorService.shutdown();
-        try {
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+	        String regex = null;
+	        System.out.println();
 
-        // Biggest ascii char from end word
-        int greatestVal = 0;
-        for (int i = 0; i < endWord.length(); i++) {
-            int curVal = (int) endWord.charAt(i);
-            if (curVal > greatestVal) {
-                greatestVal = curVal;
-            }
-        }
+	        System.out.println("START");
+	        contentList = new ArrayList<>();
 
-        char asChar = (char) greatestVal;
-        // setup regex go through start word and replace . for every index
-        // For example: cat, .at, c.t, .at
-        String regex = null;
-        System.out.println();
+	        wordOccured = new HashMap<>();
+	        path = new ArrayList<>();
+	        searchedWord = startWord;
+	        found = false;
+	        path.add(startWord);
 
-        System.out.println("START");
-        contentList = new ArrayList<>();
+	        while (found != true) {
+	            for (int i = 0; i < endWord.length(); i++) {
+	                regex = searchedWord.replace(searchedWord.charAt(i), '.');
 
-        wordOccured = new HashMap<>();
-        path = new ArrayList<>();
-        searchedWord = startWord;
-        found = false;
-        path.add(startWord);
+	                contentList = searchForMatch(oneCallableResult, regex, searchedWord);
 
-        while (found != true) {
-            for (int i = 0; i < endWord.length(); i++) {
-                regex = searchedWord.replace(searchedWord.charAt(i), '.');
+	                for (String result : contentList) {
+	                    if (result.contains(endWord)) {
+	                        path.add(endWord);
+	                        found = true;
+	                        break;
+	                    } else {
+	                        countOccurencesValue = countOccurrences(result, endWord);
+	                        wordOccured.put(result, countOccurencesValue);
+	                    }
+	                }
+	            }
 
-                contentList = searchForMatch(oneCallableResult, regex, searchedWord);
+	            searchedWord = getWord(wordOccured, endWord);
 
-                for (String result : contentList) {
-                    if (result.contains(endWord)) {
-                        path.add(endWord);
-                        found = true;
-                        break;
-                    } else {
-                        countOccurencesValue = countOccurrences(result, endWord);
-                        wordOccured.put(result, countOccurencesValue);
-                    }
-                }
-            }
-
-            searchedWord = getWord(wordOccured, endWord);
-
-            if (found == true) {
-                break;
-            }
-            System.out.println("Searched Word");
-            System.out.println(searchedWord);
-            path.add(searchedWord);
-        }
-
-        System.out.println("searchForMatch");
-        for (String str : contentList) {
-            System.out.println(str);
-        }
-
-        System.out.println("oneCallableResult");
-        for (String str : oneCallableResult) {
-            System.out.println(str);
-        }
+	            if (found == true) {
+	                break;
+	            }
+	            System.out.println("Searched Word");
+	            System.out.println(searchedWord);
+	            path.add(searchedWord);
+	        }
 
 
-        System.out.println("============================");
-        System.out.println("Found Path");
-        for (String word : path) {
+	        System.out.println("============================");
+	        System.out.println("Found Path");
+	        for (String word : path) {
 
-            System.out.println(word);
-        }
+	            System.out.println(word);
+	        }
 
-    }
+	    }
 
-    public static List<String> searchForMatch(List<String> oneCallableResult, String regex, String startWord) {
-        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
-        Matcher matcher = null;
-        boolean matchFound = false;
-        List<String> wordList = new ArrayList<>();
-        for (String s : oneCallableResult) {
-            if (!s.equals(startWord)) {
-                matcher = pattern.matcher(s);
-                matchFound = matcher.matches();
-                if (matchFound) {
-                    wordList.add(s);
-                }
-            }
-        }
-        return wordList;
-    }
+	    public static List<String> searchForMatch(List<String> oneCallableResult, String regex, String startWord) {
+	        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
+	        Matcher matcher = null;
+	        boolean matchFound = false;
+	        List<String> wordList = new ArrayList<>();
+	        for (String s : oneCallableResult) {
+	            if (!s.equals(startWord)) {
+	                matcher = pattern.matcher(s);
+	                matchFound = matcher.matches();
+	                if (matchFound) {
+	                    wordList.add(s);
+	                }
+	            }
+	        }
+	        return wordList;
+	    }
 
-    public static String getWord(Map<String, Integer> wordOccured, String endWord) {
-        int max = 0; // Biggest occurrences
-        String matchWord = null; // Word that we look for
-        Map<String, Integer> wordHelper = new HashMap<>(); // map to store values with biggest occurences
-        Map<Integer, String> wordHelperSeq = new HashMap<>(); // map to store values with biggest sequence
-        for (Map.Entry<String, Integer> pair : wordOccured.entrySet()) {
-            if (max < pair.getValue()) {
-                max = pair.getValue();
-            }
-        }
-        String keyIfOneResult = null;
-        for (Map.Entry<String, Integer> pair : wordOccured.entrySet()) {
-            if (pair.getValue() == max) {
-                keyIfOneResult = pair.getKey();
-                wordHelper.put(pair.getKey(), pair.getValue());
-            }
-        }
-        if (wordHelper.size() == 1) {
-            matchWord = keyIfOneResult;
-        } else {
-            int sequenceIndex = 0;
-            List<Integer> biggestNumber = new ArrayList<>();
-            boolean activateSequence = false;
-            for (Map.Entry<String, Integer> pair : wordHelper.entrySet()) {
-                for (int i = 0; i < endWord.length(); i++) {
-                    if (endWord.charAt(i) == pair.getKey().charAt(i)) {
-                        sequenceIndex++;
-                        activateSequence = true;
-                    } else {
-                        if (i >= 1) {
-                            if (activateSequence) {
-                                sequenceIndex--;
-                            }
-                        }
-                    }
-                }
-                biggestNumber.add(sequenceIndex);
-                wordHelperSeq.put(sequenceIndex, pair.getKey());
-                sequenceIndex = 0;
-                activateSequence = false;
-            }
-            int biggest = Collections.max(biggestNumber);
-            matchWord = wordHelperSeq.get(biggest);
-        }
-        return matchWord;
-    }
+	    public static String getWord(Map<String, Integer> wordOccured, String endWord) {
+	        int max = 0; 
+	        String matchWord = null; 
+	        Map<String, Integer> wordHelper = new HashMap<>(); 
+	        Map<Integer, String> wordHelperSeq = new HashMap<>();
+	        for (Map.Entry<String, Integer> pair : wordOccured.entrySet()) {
+	            if (max < pair.getValue()) {
+	                max = pair.getValue();
+	            }
+	        }
+	        String keyIfOneResult = null;
+	        for (Map.Entry<String, Integer> pair : wordOccured.entrySet()) {
+	            if (pair.getValue() == max) {
+	                keyIfOneResult = pair.getKey();
+	                wordHelper.put(pair.getKey(), pair.getValue());
+	            }
+	        }
+	        if (wordHelper.size() == 1) {
+	            matchWord = keyIfOneResult;
+	        } else {
+	            int sequenceIndex = 0;
+	            List<Integer> biggestNumber = new ArrayList<>();
+	            boolean activateSequence = false;
+	            for (Map.Entry<String, Integer> pair : wordHelper.entrySet()) {
+	                for (int i = 0; i < endWord.length(); i++) {
+	                    if (endWord.charAt(i) == pair.getKey().charAt(i)) {
+	                        sequenceIndex++;
+	                        activateSequence = true;
+	                    } else {
+	                        if (i >= 1) {
+	                            if (activateSequence) {
+	                                sequenceIndex--;
+	                            }
+	                        }
+	                    }
+	                }
+	                biggestNumber.add(sequenceIndex);
+	                wordHelperSeq.put(sequenceIndex, pair.getKey());
+	                sequenceIndex = 0;
+	                activateSequence = false;
+	            }
+	            int biggest = Collections.max(biggestNumber);
+	            matchWord = wordHelperSeq.get(biggest);
+	        }
+	        return matchWord;
+	    }
 
-    public static Integer countOccurrences(String word, String endWord) {
-        int k = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == endWord.charAt(i)) {
-                k++;
-            }
-        }
-        return k;
-    }
-
+	    public static Integer countOccurrences(String word, String endWord) {
+	        int k = 0;
+	        for (int i = 0; i < word.length(); i++) {
+	            if (word.charAt(i) == endWord.charAt(i)) {
+	                k++;
+	            }
+	        }
+	        return k;
+	    }
 }
 
