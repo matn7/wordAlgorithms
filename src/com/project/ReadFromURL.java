6b1f21a20e244b3e921981ad6c0cb5e52713f651
Mateusz Nowak <mateusz.nowak.199120@gmail.com>
2017-09-15 23:33:35 +0200

    First Commit

---
+++ src/com/project/ReadFromURL.java
@@ -0,0 +1,50 @@
+package com.project;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class ReadFromURL implements Callable<List<String>> {
+
+    private String stringUrl;
+    private Integer stringLength;
+    private String startString;
+    private String endString;
+    List<String> lista;
+
+
+    public ReadFromURL(String stringUrl, Integer stringLength, String startString, String endString) {
+        this.stringUrl = stringUrl;
+        this.stringLength = stringLength;
+        this.startString = startString;
+        this.endString = endString;
+    }
+
+    @Override
+    public List<String> call() throws Exception {
+        lista = new ArrayList<>();
+        URL url = new URL(stringUrl);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                if (line.length() == stringLength) {
+                        if (((int)line.charAt(0) >= (int)startString.charAt(0) && (int) line.charAt(0) <= (int) endString.charAt(0))) {
+                            lista.add(line);
+                        }
+                }
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+}
---
+++ testsrc/com/project/WordChainsTest.java
@@ -0,0 +1,113 @@
+package com.project;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordChainsTest {
+	
+	private WordChains wordChains;
+
+
+	@Before
+	public void setUp() throws Exception {
+		wordChains = new WordChains();
+	}
+	
+	@Test
+	public void testCountOccurrences() {
+		int expected = 2;
+		int value = WordChains.countOccurrences("cog", "dog");
+		assertEquals("occured passed: ", expected, value);
+	}
+	
+	@Test
+	public void testSearchForMatch() {
+		List<String> expected = new ArrayList<>();
+		expected.add("aat");
+		expected.add("bat");
+		expected.add("dat");
+		expected.add("eat");
+		
+		List<String> oneCallableResult = new ArrayList<>();
+		oneCallableResult.add("aat");
+		oneCallableResult.add("bat");
+		oneCallableResult.add("dat");
+		oneCallableResult.add("eat");		
+		oneCallableResult.add("azt");
+		oneCallableResult.add("bay");
+		oneCallableResult.add("day");
+		oneCallableResult.add("ett");
+		
+		String regex = ".at";
+		String startWord = "cat";
+		
+		List<String> value = WordChains.searchForMatch(oneCallableResult, regex, startWord);
+		
+		assertArrayEquals(expected.toArray(), value.toArray());
+		expected.clear();
+		oneCallableResult.clear();
+		
+		expected.add("cot");
+		expected.add("cbt");
+		expected.add("cqt");
+		expected.add("cet");
+		
+		oneCallableResult.add("zzz");
+		oneCallableResult.add("cot");
+		oneCallableResult.add("cbt");
+		oneCallableResult.add("cqt");
+		oneCallableResult.add("cet");		
+		oneCallableResult.add("azt");
+		oneCallableResult.add("bay");
+		oneCallableResult.add("day");
+		oneCallableResult.add("ett");
+		
+		regex = "c.t";
+		
+		value = WordChains.searchForMatch(oneCallableResult, regex, startWord);
+		assertArrayEquals(expected.toArray(), value.toArray());
+		
+	}
+	
+	@Test
+	public void testGetWord() {
+		String expected = "rode";		
+		String endWord = "ruby";		
+		Map<String, Integer> wordOccured = new HashMap<>();
+		
+		wordOccured.put("rode", 1);
+		wordOccured.put("node", 0);
+		wordOccured.put("mode", 0);
+		wordOccured.put("lode", 0);		
+		String value = WordChains.getWord(wordOccured, endWord);		
+		assertEquals("get word based on max occurrences: ", expected, value);		
+		wordOccured.clear();
+		
+		expected = "ruby";
+		wordOccured.put("ruby", 4);
+		wordOccured.put("rubs", 3);
+		value = WordChains.getWord(wordOccured, endWord);		
+		assertEquals("get word which all letter match:", expected, value);		
+		wordOccured.clear();
+		
+		expected = "goad";
+		endWord = "lead";
+		wordOccured.put("goad", 2);
+		wordOccured.put("geld", 2);
+		wordOccured.put("lald", 2);
+		value = WordChains.getWord(wordOccured, endWord);
+		assertEquals("get word based on max num of match sequence of string to endWord", expected, value);
+		
+		
+		
+		
+	}
+
+}
